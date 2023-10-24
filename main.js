014ae8206701da5c1cbfae0935cf989faf65cb57
AlbinBjerhem <albinbjerhem@gmail.com>
2023-10-24 07:31:17 +0200

    Testdata
    Testdata

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { Band } from "./addBand.js"
 import { Artist } from "./addArtist.js"
+import prompt from "prompt-sync";
+const promptSync = prompt();
 
 //Test för att se att det går att kalla på metoden från addBand
 Band.addBand('Testband', 'Testestet', 2000, 0, ['Hans', 'Kalle']);
@@ -8,3 +10,8 @@ Band.addBand('Testband', 'Testestet', 2000, 0, ['Hans', 'Kalle']);
 
 
 Artist.addArtist('Jens', 'Har en bil', 1992, 'Arn', 'Leif', ['Gitarr', 'Bas']);
+
+
+
+
+
