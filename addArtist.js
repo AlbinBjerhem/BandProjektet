48d1f27d8359b1565b1c41fdfb7f1ba7464ea645
AlbinBjerhem <albinbjerhem@gmail.com>
2023-10-24 06:27:03 +0200

    Metod för artist
    Lagt till metod för artist och testat att den skriver till json

--- addArtist.js
+++ addArtist.js
@@ -1,15 +1,12 @@
-export default Artist
+// export default Artist
+import fs from "fs";
+
+let nextArtistId = 1;
+const Artists = [];
 
 class Artist {
-
-  name;
-  infoText;
-  birthYear;
-  activeBands;
-  previousBands;
-  instruments;
-
   constructor(name, infoText, birthYear, activeBands = "", previousBands = "", instruments = []) {
+    this.id = nextArtistId++;
     this.name = name;
     this.infoText = infoText;
     this.birthYear = birthYear;
@@ -18,4 +15,19 @@ class Artist {
     this.instruments = instruments;
   }
 
+  static createArtist(name, infoText, birthYear, activeBands = "", previousBands = "", instruments = []) {
+    return new Artist(name, infoText, birthYear, activeBands, previousBands, instruments);
+  }
+
+  static addArtist(name, infoText, birthYear, activeBands, previousBands, instruments) {
+    const newArtist = new Artist(name, infoText, birthYear, activeBands, previousBands, instruments);
+    Artists.push(newArtist);
+
+    fs.writeFileSync("Artists.json", JSON.stringify(Artists, null, 2), "utf-8");
+
+    console.log("Artist added successfully.");
+  }
 }
+
+// Test data
+Artist.addArtist('Albin', 'Bor i en lägenhet', 1990, 'Håkan', 'Bengt', ['Piano', 'Trummor']);
